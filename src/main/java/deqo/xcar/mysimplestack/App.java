c0b0e80dcb9bb53bb4094de482224b6fb1da5fdb
Xcarr <xavier.carrau@univ-tlse3.fr>
2018-10-18 11:39:21 +0200

    compile

---
+++ src/main/java/deqo/xcar/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.xcar.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
